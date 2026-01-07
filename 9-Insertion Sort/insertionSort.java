325e184326f14c425c05900450eae450e78e38f1
Nikhil Pal <nikhilpal630622@mail.com>
2026-01-07 23:37:50 +0530

    including insertion sort

---
+++ 9-Insertion Sort/insertionSort.java
@@ -0,0 +1,24 @@
+// package 9-Insertion Sort;
+
+import java.util.Arrays;
+
+public class insertionSort {
+    public static void main(String[] args) {
+        int[] arr={7,5,4,3,2,1};
+        System.out.println(Arrays.toString(InsertionSort(arr)));
+    }
+    static int[] InsertionSort(int[] arr){
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=i+1;j>0;j--){
+                if(arr[j-1]>arr[j]){
+                    int temp=arr[j-1];
+                    arr[j-1]=arr[j];
+                    arr[j]=temp;
+                }else{
+                    break;
+                }
+            }
+        }
+        return arr;
+    }
+}
