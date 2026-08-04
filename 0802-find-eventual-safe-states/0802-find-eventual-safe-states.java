42d3c48d5cc69d0b2a83e4e81d4128d54c154470
Nikhil Pal <nikhilpal630622@gmail.com>
2026-08-05 01:52:00 +0530

    Time: 4 ms (99.61%), Space: 64 MB (18.03%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,40 +1,53 @@
 class Solution {
+
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        int n=graph.length;
-        List<List<Integer>> revG=new ArrayList<>();
-        int[] inD=new int[n];//in degree of nodes
-        for(int i=0;i<n;i++){
-            revG.add(new ArrayList<>());
-        }
-        // reverse the grapg directions to apply th kahn's algorithm
-        for(int i=0;i<n;i++){
-            for(int j=0;j<graph[i].length;j++){
-                inD[i]++;
-                revG.get(graph[i][j]).add(i);
-            }
-        }
-        System.out.println(revG);
-        System.out.println(Arrays.toString(inD));
-        //now applying kahn's algorithms
-        Queue<Integer> q=new LinkedList<>();
-        List<Integer> res=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            if(inD[i]==0){
-                q.add(i);
-            }
-        }
-        while(q.size()>0){
-            int front=q.remove();
-            res.add(front);
-            for(int i=0;i<revG.get(front).size();i++){
-                inD[revG.get(front).get(i)]--;
-                if(inD[revG.get(front).get(i)]==0){
-                    q.add(revG.get(front).get(i));
-                }
+
+        int n = graph.length;
+
+        boolean[] vis = new boolean[n];
+        boolean[] pathVis = new boolean[n];
+        boolean[] safe = new boolean[n];
+
+        for(int i = 0; i < n; i++){
+            if(!vis[i]){
+                dfs(i, graph, vis, pathVis, safe);
             }
         }
 
-        Collections.sort(res);
-        return res;
+        List<Integer> ans = new ArrayList<>();
+
+        for(int i = 0; i < n; i++){
+            if(safe[i]){
+                ans.add(i);
+            }
+        }
+
+        return ans;
+    }
+
+    boolean dfs(int node,
+                int[][] graph,
+                boolean[] vis,
+                boolean[] pathVis,
+                boolean[] safe){
+
+        vis[node] = true;
+        pathVis[node] = true;
+
+        for(int next : graph[node]){
+
+            if(!vis[next]){
+                if(dfs(next, graph, vis, pathVis, safe))
+                    return true;
+            }
+            else if(pathVis[next]){
+                return true;
+            }
+        }
+
+        pathVis[node] = false;
+        safe[node] = true;
+
+        return false;
     }
 }
