76b35a3d9f90b347b159aa2e5541fa0c525f85e4
Nikhil Pal <nikhilpal630622@gmail.com>
2026-08-05 00:08:11 +0530

    Time: 7 ms (28.28%), Space: 46.5 MB (95.62%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] findOrder(int n, int[][] pre) {
+         List<List<Integer>> adj=new ArrayList<>();
+        int[] inD=new int[n];//for indegree
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<pre.length;i++){
+            int v=pre[i][0];
+            int u=pre[i][1];
+            adj.get(u).add(v);
+            inD[v]++;
+        }
+        Queue<Integer> q=new LinkedList<>();
+        List<Integer> ans=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(inD[i]==0){
+                q.add(i);
+            }
+        }
+
+        while(q.size()>0){
+            int front=q.remove();
+            ans.add(front);// if all are visited then added to ans
+            for(int i=0;i<adj.get(front).size();i++){
+                inD[adj.get(front).get(i)]--;//in degree cannot be in - because the parent element is one indegree
+                if(inD[adj.get(front).get(i)]==0){//if degree is more than 0 means there any another path exist to visit this node
+                    q.add(adj.get(front).get(i));
+                }
+            }
+        }
+        if(ans.size()<n){
+            return new int[0];
+        }else{
+            int [] ret=new int[n];
+            for(int i=0;i<ans.size();i++){
+                ret[i]=ans.get(i);
+            }
+            return ret;
+        }
+    }
+}
