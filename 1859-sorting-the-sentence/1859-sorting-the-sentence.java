3230536e8416e0faeaa315ad42ed6b6ea291416e
Nikhil Pal <nikhilpal630622@gmail.com>
2026-08-08 22:24:36 +0530

    Time: 0 ms (100%), Space: 43 MB (28.08%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.java
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -6,13 +6,7 @@ class Solution {
             int ind = arr[i].charAt(arr[i].length() - 1) - '1';
             res[ind] = arr[i].substring(0, arr[i].length() - 1);
         }
-        System.out.println(Arrays.toString(res));
-        StringBuilder st=new StringBuilder();
-        for(int i=0;i<res.length;i++){
-            st.append(res[i]);
-            if(i<res.length-1){st.append(" ");
-            }
-        }
-        return st.toString();
+       
+        return String.join(" ",res);
     }
 }
