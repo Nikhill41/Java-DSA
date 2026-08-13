f3a5a3f9feaf9238791200d78077fdb7abd07b23
Nikhil Pal <nikhilpal630622@gmail.com>
2026-08-14 00:01:35 +0530

    Time: 0 ms (100%), Space: 42.3 MB (16.16%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,7 +1,20 @@
 class Solution {
     public int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
-        return fib(n-1)+fib(n-2);
+        int[] res=new int[n+1];
+        fibo(n,res);
+        return res[n];
+    }
+    int fibo(int n, int[] res){
+        if(n<=1){
+            if(n>=0){
+                res[n]=n;
+            }
+            return n;
+        }
+        if(res[n]!=0) return res[n];
+        int negative1=fibo(n-1,res);
+        int negative2=fibo(n-2,res);
+        res[n]=negative1+negative2;
+        return negative1+negative2;
     }
 }
